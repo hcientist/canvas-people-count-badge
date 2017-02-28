a7fe532e9e6b1a641d070e2c7c92323992ec31fe
Michael Stewart <tgm@vt.edu>
2017-02-27 19:02:32 -0500

    v0.2

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 // ==UserScript==
 // @name         Add users count to people nav when on users page
 // @namespace    http://canvas.vt.edu/
-// @version      0.1
+// @version      0.2
 // @description  enter something useful
 // @author       You
 // @match        https://canvas.vt.edu/courses/*/users
 // @grant        none
 // ==/UserScript==
 $(function () {
-window.setTimeout(function(){
+    var timerId = window.setInterval(function(){
   function addBadge () {
     if ($('table.roster tbody tr').length > 0) {
       $('nav li.section a.people').append('<span class="badge pull-right">'+$('table.roster tbody tr').length+'</span>');
     }
   }
-  if ($._data(window).events.scroll && $._data(window).events.scroll.length > 0) {
+  if ($._data(window).events.scroll && $._data(window).events.scroll.length > 0 && $('.paginatedLoadingIndicator').is(":visible"))  {
     $._data(window).events.scroll[0].handler();
-    window.setTimeout(addBadge, 1000);
   } else {
     addBadge();
   }
-}, 2500);
+}, 250);
 });
